ff76d898b3b76c5ef0a469007858b969a7c4883e
Vasil Sariev <116823112+vasilsariev@users.noreply.github.com>
2024-10-17 19:03:24 +0300

    Delete send-btc/target directory

--- send-btc/target/debug/deps/send_btc-c41ac6f960ab576e.d
+++
@@ -1,5 +0,0 @@
-C:\Users\vasa\WORK\rust\send-btc\target\debug\deps\libsend_btc-c41ac6f960ab576e.rmeta: src/main.rs
-
-C:\Users\vasa\WORK\rust\send-btc\target\debug\deps\send_btc-c41ac6f960ab576e.d: src/main.rs
-
-src/main.rs:
